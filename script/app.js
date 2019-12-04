0237732c3325f878f160c264937f3354c6d83efc
Emile Goeminne <emile.goeminne@student.howest.be>
2019-12-04 20:14:46 +0100

    Big update, added countdown visual more appealing

--- script/app.js
+++ script/app.js
@@ -14,15 +14,18 @@ const fetchData = function (url) {
 const getData = async function (url) {
     try {
         let data = await fetchData(url);
-        console.log(data);
+        //console.log(data);
         return data;
     } catch (error) {
         console.error(error);
     }
 };
 
-const GetSols = async function () {
+const LoadInfo = async function () {
     dataArray = [];
+    let htmltemp = "";
+    let htmlpres = "";
+    let htmlwind = "";
     let html = "";
     let url = `https://api.nasa.gov/insight_weather/?api_key=8gDyVN1GWGv6QnHf3ZMxLwKvBQcQB4k4pteteai6&feedtype=json&ver=1.0`;
     let data = await fetchData(url);
@@ -30,22 +33,9 @@ const GetSols = async function () {
         dataArray.push(sol);
     }
     for (i = 0; i < dataArray.length; i++) {
-        html += `<h3 class="u-margin__0 js-daySelected">` +  dataArray[i] + `</h3>`;
-    };
-    document.querySelector('.js-sols').innerHTML = html;
-
-}
-
-const LoadInfo = async function () {
-    let htmltemp = "";
-    let htmlpres = "";
-    let htmlwind = "";
-    let url = `https://api.nasa.gov/insight_weather/?api_key=8gDyVN1GWGv6QnHf3ZMxLwKvBQcQB4k4pteteai6&feedtype=json&ver=1.0`;
-    let data = await fetchData(url);
-    for (i = 0; i < dataArray.length; i++) {
         if (dataArray[i] == dataArray[0]){
+            html += `<h3 class="u-margin__0 js-daySelected">` +  dataArray[i] + `</h3>`;
             let number = dataArray[i];
-            console.log(data[number]);
             htmltemp += `<h2 class="o-layout--align-center">` + Math.floor(data[number].AT.av) + `</h2>
             <div class="o-layout o-layout--justify-space-between">
                 <p>C</p>
@@ -88,8 +78,11 @@ const LoadInfo = async function () {
                     <p>`+ Math.floor(data[number].PRE.mx) +`</p>
                 </div>
             </div>`
+        }else{
+            html += `<h3 class="c-not-selected u-margin__0 js-daySelected">` +  dataArray[i] + `</h3>`;
         }
     };
+    document.querySelector('.js-sols').innerHTML = html;
     document.querySelector('.js-temperature').innerHTML = htmltemp;
     document.querySelector('.js-pres').innerHTML = htmlpres;
     document.querySelector('.js-wind').innerHTML = htmlwind;
@@ -100,12 +93,13 @@ const infoByDay = async function (day) {
     let htmltemp = "";
     let htmlpres = "";
     let htmlwind = "";
+    let html = "";
     let url = `https://api.nasa.gov/insight_weather/?api_key=8gDyVN1GWGv6QnHf3ZMxLwKvBQcQB4k4pteteai6&feedtype=json&ver=1.0`;
     let data = await fetchData(url);
     for (i = 0; i < dataArray.length; i++) {
         if (dataArray[i] == day){
+            html += `<h3 class="u-margin__0 js-daySelected">` +  dataArray[i] + `</h3>`;
             let number = dataArray[i];
-            console.log(data[number]);
             htmltemp += `<h2 class="o-layout--align-center">` + Math.floor(data[number].AT.av) + `</h2>
             <div class="o-layout o-layout--justify-space-between">
                 <p>C</p>
@@ -148,20 +142,46 @@ const infoByDay = async function (day) {
                     <p>`+ Math.floor(data[number].PRE.mx) +`</p>
                 </div>
             </div>`
+        }else{
+            html += `<h3 class="c-not-selected u-margin__0 js-daySelected">` +  dataArray[i] + `</h3>`;
         }
     };
+    document.querySelector('.js-sols').innerHTML = html;
     document.querySelector('.js-temperature').innerHTML = htmltemp;
     document.querySelector('.js-pres').innerHTML = htmlpres;
     document.querySelector('.js-wind').innerHTML = htmlwind;
 
 }
 
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Dec 20, 2019 00:00:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+
+    }, second)
+
 
 /*Event listeners*/
 
 const addEventListeners = function () {
     sols.addEventListener('click', function (e) {
-        console.log(e.target.innerText);
         infoByDay(e.target.innerText);
     });
 };
@@ -170,7 +190,6 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
     daySelect = document.querySelector('.js-daySelected');
     sols = document.querySelector('.js-sols');
-    GetSols();
     LoadInfo();
     addEventListeners();
 });
